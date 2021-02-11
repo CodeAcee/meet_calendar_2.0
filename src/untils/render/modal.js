1f125e24a6ade41adcb97f29d71ede93309fcb5a
Yaroslav Pikiner <yaroyay@gmail.com>
2021-02-11 13:40:41 +0200

    add modal

--- src/untils/render/modal.js
+++ src/untils/render/modal.js
@@ -1,11 +1,15 @@
 export const modal = (name) => {
     const modalWrapper = document.createElement('div');
+    const btnWrapper = document.createElement('div');
     const modalTitle = document.createElement('p');
+    
     const modalBtnYes = document.createElement('button');
     const modalBtnNo = document.createElement('button');
 
     modalWrapper.setAttribute('class', 'modal');
+    btnWrapper.setAttribute('class', 'modal__btn__wrapper');
     modalTitle.setAttribute('class', 'modal__title');
+
     modalBtnYes.setAttribute('class', 'btn modal__btn--yes');
     modalBtnNo.setAttribute('class', 'btn modal__btn--no');
 
@@ -13,9 +17,10 @@ export const modal = (name) => {
     modalBtnYes.textContent = 'Yes';
     modalBtnNo.textContent = 'No';
 
+    btnWrapper.append(modalBtnYes);
+    btnWrapper.append(modalBtnNo);
     modalWrapper.append(modalTitle);
-    modalWrapper.append(modalBtnYes);
-    modalWrapper.append(modalBtnNo);
+    modalWrapper.append(btnWrapper);
 
     return modalWrapper;
 }   
